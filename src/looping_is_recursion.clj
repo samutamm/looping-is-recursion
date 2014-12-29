f660581ec920286748f87f98dffab8aa2ef0397f
samutamm <samutamm@gmail.com>
2014-12-29 14:02:25 +0200

    1 & 2

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,10 +1,18 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (let [helper (fn [acc n]
+                 (if (>= 1 n)
+                   acc
+                   (recur (* acc base) (dec n))))]
+    (helper base exp)))
 
 (defn last-element [a-seq]
-  ":(")
+  (let [helper (fn [a-sequence]
+                 (if (or (= (count a-sequence) 1) (empty? a-sequence))
+                   (first a-sequence)
+                   (recur (rest a-sequence))))]
+    (helper a-seq)))
 
 (defn seq= [seq1 seq2]
   ":(")
